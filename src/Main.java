b87242539109c4994d6c78f072f0c0422789c2e6
Diana Dinh-Andrus <diana.dinhandrus@revature.net>
2024-01-30 17:03:02 -0600

    Added Solution and Tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[] chars1 = {'a','a','b','b','c','c','c'};
+        char[] expectedChars1 = {'a','2','b','2','c','3'};
+        System.out.println(testSolution(chars1, expectedChars1));
+
+        char[] chars2 = {'a'};
+        char[] expectedChars2 = {'a'};
+        System.out.println(testSolution(chars2, expectedChars2));
+
+        char[] chars3 = {'a','b','b','b','b','b','b','b','b','b','b','b','b'};
+        char[] expectedChars3 = {'a','b','1','2'};
+        System.out.println(testSolution(chars3, expectedChars3));
+    }
+
+    private static boolean testSolution(char[] chars, char[] expectedChars){
+        Solution solution = new Solution();
+        int k = solution.compress(chars);
+        if(k != expectedChars.length)
+                return false;
+        for(int i = 0; i < expectedChars.length; i++)
+            if(chars[i] != expectedChars[i])
+                return false;
+        return true;
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,36 @@
-public class Solution {
+class Solution {
+    public int compress(char[] chars) {
+        int pointer = 0;
+        char c = chars[0];
+        int count = 1;
+        for(int i = 1; i < chars.length; i++){
+            char temp = chars[i];
+            if(temp == c){
+                count++;
+            }
+            else{
+                chars[pointer++] = c;
+                if(count > 999)
+                    chars[pointer++] = Character.forDigit(count / 1000,10);
+                if(count > 99)
+                    chars[pointer++] =  Character.forDigit(count / 100 % 10,10);
+                if(count > 9)
+                    chars[pointer++] =  Character.forDigit(count / 10 % 10,10);
+                if(count > 1)
+                    chars[pointer++] =  Character.forDigit(count % 10,10);
+                count = 1;
+                c = temp;
+            }
+        }
+        chars[pointer++] = c;
+        if(count > 999)
+            chars[pointer++] = Character.forDigit(count / 1000,10);
+        if(count > 99)
+            chars[pointer++] =  Character.forDigit(count / 100 % 10,10);
+        if(count > 9)
+            chars[pointer++] =  Character.forDigit(count / 10 % 10,10);
+        if(count > 1)
+            chars[pointer++] =  Character.forDigit(count % 10,10);
+        return pointer;
+    }
 }
